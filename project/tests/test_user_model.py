964e512e1690b7b402852e52164bd10c8213223a
tj <tbindseil@yahoo.com>
2020-11-08 15:22:50 +0000

    change user modul error handling and tests

--- project/tests/test_user_model.py
+++ project/tests/test_user_model.py
@@ -3,7 +3,7 @@ from unittest.mock import patch, MagicMock
 import jwt
 
 from project.server import db
-from project.server.models import User
+from project.server.models import User, BlacklistToken
 from project.tests.base import BaseTestCase
 
 
@@ -24,17 +24,30 @@ class TestUserModel(BaseTestCase):
         auth_token = self.user.encode_auth_token(self.user.id)
         self.assertTrue(isinstance(auth_token, bytes))
 
-    @patch('jwt.encode')
-    def test_encode_auth_token_throws(self, mock_encode):
-        mock_encode.side_effect = Exception
-
-        output = self.user.encode_auth_token(self.user.id)
-        assert(output, Exception)
-
     def test_decode_auth_token(self):
         auth_token = self.user.encode_auth_token(self.user.id)
         self.assertTrue(isinstance(auth_token, bytes))
         self.assertTrue(User.decode_auth_token(auth_token.decode("utf-8")) == 1)
+
+    @patch('project.server.models.BlacklistToken.check_blacklist')
+    def test_decode_auth_token_blacklisted_token(self, mock_check_blacklist):
+        is_blacklisted_token = True
+
+        mock_check_blacklist.return_value = is_blacklisted_token
+
+        auth_token = self.user.encode_auth_token(self.user.id)
+        self.assertTrue(isinstance(auth_token, bytes))
+        self.assertTrue(User.decode_auth_token(auth_token.decode("utf-8")) ==
+                        "Token blacklisted. Please log in again.")
+
+    @patch('jwt.decode')
+    def test_decode_auth_token_expired_token(self, mock_decode):
+        mock_decode.side_effect = jwt.ExpiredSignatureError
+
+        auth_token = self.user.encode_auth_token(self.user.id)
+        self.assertTrue(isinstance(auth_token, bytes))
+        self.assertTrue(User.decode_auth_token(auth_token.decode("utf-8")) ==
+                        "Signature expired. Please log in again.")
 
     @patch('jwt.decode')
     def test_decode_auth_token_invalid_token(self, mock_decode):
@@ -44,6 +57,7 @@ class TestUserModel(BaseTestCase):
         self.assertTrue(isinstance(auth_token, bytes))
         self.assertTrue(User.decode_auth_token(auth_token.decode("utf-8")) ==
                        "Invalid token. Please log in again.")
+
 
 if __name__ == '__main__':
     unittest.main()
